e3668cd1ae6dced57edebc1f13317ab317782847
sj0020 <jungej0106@gmail.com>
2022-10-13 15:37:21 -0600

    feat: add 'Sarah-component.vue' page

---
+++ quasar-project/src/pages/Sarah-component.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>Hello from Sarah</h3>
+    <q-banner class="bg-primary text-white">
+      Unfortunately, the credit card did not go through, please try again.
+      <template v-slot:action>
+        <q-btn flat color="white" label="Dismiss" />
+        <q-btn flat color="white" label="Update Credit Card" />
+      </template>
+    </q-banner>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
